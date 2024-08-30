94324695e68ea8b724ae05198bb1bcd24717be6a
Alexandr Melnikov <also12225@gmail.com>
2024-08-30 17:49:59 +0300

    task1 - completed the main part of the task

---
+++ src/main/java/ru/melnikov/task/credit/service/services/impl/SystemManagementServiceImpl.java
@@ -0,0 +1,51 @@
+package ru.melnikov.task.credit.service.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.melnikov.task.credit.service.dao.SystemManagementRepository;
+import ru.melnikov.task.credit.service.dto.request.ClientPersonalDataRequestDto;
+import ru.melnikov.task.credit.service.dto.response.ClientResponseDto;
+import ru.melnikov.task.credit.service.dto.response.LoanAgreementResponseDto;
+import ru.melnikov.task.credit.service.dto.response.LoanApplicationResponseDto;
+import ru.melnikov.task.credit.service.services.SystemManagementService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SystemManagementServiceImpl implements SystemManagementService {
+
+    private final SystemManagementRepository managementRepository;
+
+    @Override
+    public List<ClientResponseDto> findClientByPersonalData(ClientPersonalDataRequestDto requestDto) {
+        return managementRepository.findClientByClientPersonalDataRequestDto(requestDto);
+    }
+
+    @Override
+    public List<ClientResponseDto> getAllClients(int page, int size) {
+        return managementRepository.getAllClients(page, size);
+    }
+
+    @Override
+    public List<LoanAgreementResponseDto> getAllAgreements(int page, int size) {
+        return managementRepository.getAllAgreements(page, size);
+    }
+
+    @Override
+    public List<LoanApplicationResponseDto> getAllApplications(int page, int size) {
+        return managementRepository.getAllApplications(page, size);
+    }
+
+    public long getTotalApplicationsCount() {
+        return managementRepository.getTotalApplicationsCount();
+    }
+
+    public long getTotalAgreementsCount() {
+        return managementRepository.getTotalAgreementsCount();
+    }
+
+    public long getTotalClientsCount() {
+        return managementRepository.getTotalClientsCount();
+    }
+}
